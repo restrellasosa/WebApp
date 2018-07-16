0f047255c3c4da48ee9dac08b317d4cd6ac618fb
Rigoberto Estrella Sosa <estrella_rigoberto@hotmail.com>
2018-07-16 08:36:25 -0400

    Hasta autorizacion

--- WebApp/Data/Migrations/20180716122425_[Migracion-Inicial].Designer.cs
+++ WebApp/Data/Migrations/20180716122425_[Migracion-Inicial].Designer.cs
@@ -11,9 +11,10 @@ using WebApp.Data;
 namespace WebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180716122425_[Migracion-Inicial]")]
+    partial class MigracionInicial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
